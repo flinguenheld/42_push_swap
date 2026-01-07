e7ce5119286a524d110a836754371b1fae4490dc
flinguen <florent@linguenheld.net>
2026-01-07 12:48:22 +0100

    commands - Only print ss rr rrr when effective

--- commands/commands.h
+++ commands/commands.h
@@ -34,6 +34,7 @@ void	swap_b(t_list *b);
 /**
  * @brief
  * Launch swap_a & swap_b
+ * Print "ss" if one swap was effective
  */
 void	swap_ab(t_list *a, t_list *b);
 
@@ -42,6 +43,7 @@ void	swap_ab(t_list *a, t_list *b);
 /**
  * @brief
  * Take the first element at the top of b and put it at the top of a.
+ * And print "pa"
  * Do nothing if b is empty
  */
 void	push_a(t_list **a, t_list **b);
@@ -49,6 +51,7 @@ void	push_a(t_list **a, t_list **b);
 /**
  * @brief
  * Take the first element at the top of a and put it at the top of b.
+ * And print "pb"
  * Do nothing if a is empty
  */
 void	push_b(t_list **a, t_list **b);
@@ -57,21 +60,24 @@ void	push_b(t_list **a, t_list **b);
 // ----------------------------------------------------------------- ROTATE ---
 /**
  * @brief
- * Shift up all elements of list a by one.
+ * Shift up all elements of list a by one and print "ra".
  * The first element becomes the last one.
+ * Do nothing if a is empty
  */
 void	rotate_a(t_list **a);
 
 /**
  * @brief
- * Shift up all elements of list b by one.
+ * Shift up all elements of list b by one and print "rb".
  * The first element becomes the last one.
+ * Do nothing if b is empty
  */
 void	rotate_b(t_list **b);
 
 /**
  * @brief
  * Launch rotate_a & rotate_b
+ * Print "rr" if one rotate was effective
  */
 void	rotate_ab(t_list **a, t_list **b);
 
@@ -79,21 +85,24 @@ void	rotate_ab(t_list **a, t_list **b);
 // --------------------------------------------------------- REVERSE_ROTATE ---
 /**
  * @brief
- * Shift down all elements of list a by one.
+ * Shift down all elements of list a by one and print "rra".
  * The last element becomes the first one.
+ * Do nothing if a is empty
  */
 void	reverse_rotate_a(t_list **a);
 
 /**
  * @brief
- * Shift down all elements of list b by one.
+ * Shift down all elements of list b by one and print "rrb".
  * The last element becomes the first one.
+ * Do nothing if a is empty
  */
 void	reverse_rotate_b(t_list **b);
 
 /**
  * @brief
  * Launch reverse_rotate_a & reverse_rotate_b
+ * Print "rrr" if one rotate was effective
  */
 void	reverse_rotate_ab(t_list **a, t_list **b);
 
