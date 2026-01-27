ea35ab698aafd2f14aa65cc95ab6ff13efae981f
flinguen <florent@linguenheld.net>
2026-01-27 11:12:52 +0100

    Patrick - Clean & comment values to keep

--- patrick.c
+++ patrick.c
@@ -16,25 +16,12 @@
 
 /**
  * @brief
- * Find the first subsequence which is higher than 'value'
- * The subsequence has to be sorted
+ * Loop in list and check if the current list value is contained in subseq
  * @return
- * The first node of the pair
- * NULL if the value is already higher than any subsequence value
+ * The first node found
+ * NULL is not found
  */
-// t_list	*get_next_sub(t_list *subseq, int value)
-// {
-// 	while (subseq != NULL)
-// 	{
-// 		if (value > content(subseq)
-// 			&& (subseq->next == NULL || value < content(subseq->next)))
-// 			return (subseq);
-// 		subseq = subseq->next;
-// 	}
-// 	return (NULL);
-// }
-
-t_list	*get_next_sub(t_list *list, t_list *subseq)
+static t_list	*get_next_subsequence_value(t_list *list, t_list *subseq)
 {
 	while (list != NULL)
 	{
@@ -45,66 +32,51 @@ t_list	*get_next_sub(t_list *list, t_list *subseq)
 	return (NULL);
 }
 
-t_list *list_values_to_keep(t_stack *a, t_list *subsequences)
+/**
+ * @brief
+ * Clear and fill the keeplist
+ * The values to keep are values which can be put in the next subsequence pair
+ * So:
+ *   - get the next subequence pair (left & right)
+ *   - loop in the stack until the 'left'
+ *       - save values which can fit in the pair
+ */
+static void	list_values_to_keep(t_stack *a, t_list *subseq, t_list **keeplist)
 {
-	// Get the next pair
-	t_list *iter;
-	t_list *brand_new_list;
-	t_list *left;
-	t_list *right;
+	t_list	*iter;
+	t_list	*left;
+	t_list	*right;
 
 	iter = a->start;
-	brand_new_list = NULL;
-	left = get_next_sub(a->start, subsequences);
+	ft_lst_clear(keeplist, free);
+	left = get_next_subsequence_value(a->start, subseq);
 	if (left != NULL)
 	{
-		// ft_printf("next pair is: %d\n", content(left));
-		right = get_next_sub(left->next, subsequences);
+		right = get_next_subsequence_value(left->next, subseq);
 		while (iter != left)
 		{
 			if (content(iter) > content(left)
 				&& (right == NULL || content(iter) < content(right)))
 			{
-				ft_lst_push_front(&brand_new_list,
+				ft_lst_push_front(keeplist,
 					ft_lst_new(new_content(content(iter))));
 			}
 			iter = iter->next;
 		}
 	}
-	if (brand_new_list != NULL )
-	{
-		// ft_printf("HERE THERE ARE %d values !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n", ft_lst_size(brand_new_list));
-		// print_list(brand_new_list, "  ");
-	}
-	return (brand_new_list);
 }
 
-void	blah(t_stack *a, t_stack *b, t_list *subsequence, t_list *keeplist)
-{
-	
-}
-
-
-
-void	only_keep_subsequence(t_stack *a, t_stack *b, t_list *subsequence, t_list *keeplist, char keeplist_status)
+void	only_keep_subsequence(t_stack *a, t_stack *b, t_list *subsequence, t_list **keeplist, char keeplist_status)
 {
 	int	keeplist_len;
 	int	to_loop;
 
-	// if (is_sorted_tolerance(a->start, 1))
 	if (is_sorted_circular(a->start) == 1)
-	{
 		return ;
-	}
 
-	// ft_printf("here we are to analyse %d\n", content(a->start));
-	// print_ab(a->start, b->start, "in progress");
-	// print_list(subsequence, " ");
-
-	keeplist_len = ft_lst_size(keeplist);
+	keeplist_len = ft_lst_size(*keeplist);
 	if (ft_lst_contains_key(subsequence, a->start->content, are_equal))
 	{
-		// ft_printf("Yep it is in subsequence\n");
 		if (keeplist_len == 0)
 			rotate(&a->start, a->name);
 		else
@@ -121,28 +93,21 @@ void	only_keep_subsequence(t_stack *a, t_stack *b, t_list *subsequence, t_list *
 			to_loop = keeplist_len + 1;
 			while (to_loop--)
 				rotate(&a->start, a->name);
-			ft_lst_clear(&keeplist, free);
+			ft_lst_clear(keeplist, free);
 		}
 		return only_keep_subsequence(a, b, subsequence, keeplist, 0);
 	}
 
 	if (keeplist_status == 0)
-	{
-		keeplist = list_values_to_keep(a, subsequence);
-	}
+		list_values_to_keep(a, subsequence, keeplist);
 	else
 	{
-		if (ft_lst_contains_key(keeplist, a->start->content, are_equal))
+		if (ft_lst_contains_key(*keeplist, a->start->content, are_equal))
 		{
-			if (ft_lst_contains_key(keeplist, a->start->next->content, are_equal))
-			{
+			if (ft_lst_contains_key(*keeplist, a->start->next->content, are_equal))
 				rotate(&a->start, a->name);
-			}
 			else
-			{
 				swap(a->start, a->name);
-			}
-			
 		}
 		else
 			push(&a->start, &b->start, b->name);
@@ -156,13 +121,15 @@ void	patrick(t_stack *a, t_stack *b)
 	t_list	*sub;
 	t_list	*keeplist;
 
+	keeplist = NULL;
 	sub = longest_sequence(a->start);
 	print_ab(sub, NULL, "LONGEST SUB");
 	// ft_printf("size %d-------------------------------------------------------\n", ft_lst_size(sub));
 	// print_ab(a->start, b->start, "is it ok ?");
 
-	only_keep_subsequence(a, b, sub, keeplist, 0);
+	only_keep_subsequence(a, b, sub, &keeplist, 0);
 	print_ab(a->start, b->start, "PUSH DONE");
+	ft_lst_clear(&keeplist, free);
 
 	ft_lst_clear(&sub, free);
 }
