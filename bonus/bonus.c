9bb10491d4cfa96c4fcfe4c365aeddc84d227c1d
flinguen <florent@linguenheld.net>
2026-01-28 12:17:21 +0100

    Bonus - add read

--- bonus/bonus.c
+++ bonus/bonus.c
@@ -11,6 +11,89 @@
 /* ************************************************************************** */
 
 #include "bonus.h"
+#include <unistd.h>
+
+static char	apply_rotate(char *txt, t_stack *a, t_stack *b)
+{
+	if (ft_strncmp(txt, "ra\n\0" , 4) == 0)
+		rotate(&a->start, '\0');
+	else if (ft_strncmp(txt, "rb\n\0" , 4) == 0)
+		rotate(&b->start, '\0');
+	else if (ft_strncmp(txt, "rr\n\0" , 4) == 0)
+	{
+		rotate(&a->start, '\0');
+		rotate(&b->start, '\0');
+	}
+	else if (ft_strncmp(txt, "rra\n" , 4) == 0)
+		reverse_rotate(&a->start, '\0');
+	else if (ft_strncmp(txt, "rrb\n" , 4) == 0)
+		reverse_rotate(&b->start, '\0');
+	else if (ft_strncmp(txt, "rrr\n" , 4) == 0)
+	{
+		reverse_rotate(&a->start, '\0');
+		reverse_rotate(&b->start, '\0');
+	}
+	else
+		return (0);
+	return (1);
+}
+
+static char	apply(char *txt, t_stack *a, t_stack *b)
+{
+	if (ft_strncmp(txt, "sa\n\0" , 4) == 0)
+		swap(a->start, '\0');
+	else if (ft_strncmp(txt, "sb\n\0" , 4) == 0)
+		swap(b->start, '\0');
+	else if (ft_strncmp(txt, "ss\n\0" , 4) == 0)
+	{
+		swap(a->start, '\0');
+		swap(b->start, '\0');
+	}
+	else if (ft_strncmp(txt, "pa\n\0" , 4) == 0)
+		push(&b->start, &a->start, '\0');
+	else if (ft_strncmp(txt, "pb\n\0" , 4) == 0)
+		push(&a->start, &b->start, '\0');
+	else
+		return (apply_rotate(txt, a, b));
+	return (1);
+}
+
+static t_list *read_that(t_stack *a, t_stack *b)
+{
+	char	buffer[5];
+	int		amount_read;
+
+	ft_memset(buffer, '\0', 5);
+	while (1)
+	{
+		amount_read = read(0, &buffer, 5);
+		if (amount_read == -1)
+		{
+			ft_printf_err("Error\n");
+			break ;
+		}
+		if (amount_read == 0)
+		{
+			ft_printf("Done\n");
+			break ;
+		}
+		if (amount_read > 4)
+		{
+			ft_printf_err("supp to 4\n");
+			break ;
+		}
+
+		if (!apply(buffer, a, b))
+		{
+			ft_printf_err("Error\n");
+			return (0);
+		}
+
+		// ft_memset(buffer, '\0', 5);
+	}
+
+	return (NULL);
+}
 
 int	main(int argc, char **argv)
 {
@@ -33,6 +116,10 @@ int	main(int argc, char **argv)
 	}
 
 	ft_printf("Hello\n");
+	print_list(a.start, " ");
+
+	read_that(&a, &b);
+
 	
 	ft_lst_clear(&a.start, free);
 	ft_lst_clear(&b.start, free);
